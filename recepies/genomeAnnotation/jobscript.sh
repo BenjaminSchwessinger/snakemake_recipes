e6496271e6004667b55242f52e88a1fd888e5bbf
Benjamin Schwessinger <bxs800@gadi-login-06.gadi.nci.org.au>
2021-04-19 21:32:43 +1000

    added the PERLIB5 after having issues with perl modules

--- recepies/genomeAnnotation/jobscript.sh
+++ recepies/genomeAnnotation/jobscript.sh
@@ -5,6 +5,8 @@ source /home/800/bxs800/scripts/snakemake/recepies/genomeAnnotation/gadimod.sh
 useconda
 conda activate funannotate
 
+export PERL5LIB=/g/data/xf3/bxs800/miniconda3/envs/funannotate/lib/site_perl/5.26.2/x86_64-linux-thread-multi:/g/data/xf3/bxs800/miniconda3/envs/funannotate/lib/site_perl/5.26.2:/g/data/xf3/bxs800/miniconda3/envs/funannotate/lib/5.26.2/x86_64-linux-thread-multi:/g/data/xf3/bxs800/miniconda3/envs/funannotate/lib/5.26.2:.
+
 export TMPDIR=$PBS_JOBFS
 export OMP_NUM_THREADS=32
 
